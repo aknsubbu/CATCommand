635c408e0b73d8a678845d9c7e6833b38214bbc9
EgoisticPotato <egoistic.potato@gmail.com>
2025-07-17 23:07:18 +0530

    Added alertService.ts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -47,6 +47,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="tray.and.arrow.down.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="Alerts"
+        options={{
+          title: 'Alerts',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="exclamationmark.triangle.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
